bfc5b6797474a7eaf3b7fd6e8766a812a32ba87e
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-01 16:25:50 +0530

    Creating a new transaction,

--- blockchain.js
+++ blockchain.js
@@ -1,20 +1,34 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
+  this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash,
   };
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock);
   return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+  const newTransactions = {
+    amount,
+    sender,
+    recipient,
+  };
+  this.pendingTransactions.push(newTransactions);
+  return this.getLastBlock()["index"] + 1;
 };
 
 module.exports = Blockchain;
--- test.js
+++ test.js
@@ -3,7 +3,11 @@ const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(285, "DUMMYHASH0", "DUMMYHADH2");
-bitcoin.createNewBlock(280, "DUMMYHASH2", "DUMMYHADH3");
-bitcoin.createNewBlock(288, "DUMMYHASH3", "DUMMYHADH4");
+bitcoin.createNewTransaction(100, "DUMMYHASH2", "DUMMYHADH3");
+bitcoin.createNewBlock(123, "DUMMYHASH4", "DUMMYHADH5");
+bitcoin.createNewTransaction(100, "DUMMYHASH2", "DUMMYHADH3");
+bitcoin.createNewTransaction(100, "DUMMYHASH2", "DUMMYHADH3");
+bitcoin.createNewTransaction(100, "DUMMYHASH2", "DUMMYHADH3");
+bitcoin.createNewBlock(123, "DUMMYHASH4", "DUMMYHADH5");
 
-console.log(bitcoin);
+console.log(JSON.stringify(bitcoin, null, 2));
